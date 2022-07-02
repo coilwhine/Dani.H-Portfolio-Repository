24e8d47741e627b655596e9cfa801f61303b8264
Daniel Hen <hen.daniel47@gmail.com>
2022-07-02 21:07:02 +0300

    add after to contact button

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
 // ---- contact me changes ----
 // document.querySelector(".contact-p").innerHTML = "message received! 👍<br>I'll contact you in the next 24 hours";
+
+
+function myFunction() {
+    console.log(fullName);
+}
+
+myFunction()
