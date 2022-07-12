4d1029391ddb3f16ffe6b0fcb2a788e85738c9d2
Daniel Hen <hen.daniel47@gmail.com>
2022-07-12 20:42:00 +0300

    submit text

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 
 // ---- contact me changes ----
 
-function submitContactMe() {
+function submitContactMe(event) {
 
-    let message = document.querySelector('.contact-p').innerHTML = "Message received! 👍<br>I'll contact you in the next 24 hours";
+    event.preventDefault();
+
+    const firstName = document.getElementById('fullName')
+    const email = document.getElementById('email')
+    const phoneNumber = document.getElementById('phoneNumber')
+
+    if (firstName.value === '') {
+        firstName.focus()
+        return false
+    }
+
+    if (email.value === '') {
+        email.focus()
+        return false
+    }
+
+    if (phoneNumber.value === '') {
+        phoneNumber.focus()
+        return false
+    }
+
+    document.querySelector('.hero-form').reset();
+
+    document.querySelector('.contact-p').innerHTML = "Message received! 👍<br>I'll contact you in the next 24 hours";
+
+    console.log(event)
 
 }
